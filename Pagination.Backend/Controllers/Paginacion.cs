aeb7e4582903f65f1be64b71f0ca460a63fec26e
Leonardo Chuello <ljchuello@gmail.com>
2021-03-12 22:15:51 -0500

    .

---
+++ Pagination.Backend/Controllers/Paginacion.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+
+namespace Pagination.Backend.Controllers
+{
+    /// <summary>
+    /// Objeto para paginar
+    /// </summary>
+    public class Paginacion
+    {
+        /// <summary>
+        /// Parámetro búsqueda
+        /// </summary>
+        public string Q { set; get; } = string.Empty;
+
+        /// <summary>
+        /// Indica la página actual
+        /// </summary>
+        public int PageCurrent { set; get; } = 0;
+
+        /// <summary>
+        /// Indica el tamaño de la paginación
+        /// </summary>
+        public int PageSize { set; get; } = 10;
+
+        /// <summary>
+        /// Indica cuantas páginas hay en total
+        /// </summary>
+        public int PageCount { set; get; } = 0;
+
+        /// <summary>
+        /// Datos devueltos
+        /// </summary>
+        public DataTable Data { set; get; } = new DataTable();
+    }
+}
---
+++ Pagination.Backend/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pagination.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public Paginacion PagIn([FromBody]Paginacion q)
+        {
+            Paginacion paginacionOut = new Paginacion();
+            return paginacionOut;
+        }
+    }
+}
